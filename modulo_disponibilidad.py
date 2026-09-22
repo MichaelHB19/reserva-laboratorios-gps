ecbec6a6811c4349f1682b2c3b5603c4401bb224
Michael <mhurtadob@unsa.edu.pe>
2026-09-22 00:29:00 -0500

    feat: implementar consulta de horarios de laboratorios (Refs OP WP#3)

---
+++ modulo_disponibilidad.py
@@ -0,0 +1,4 @@
+# modulo_de_disponibilidad
+def consultar_disponibilidad(laboratorio_id, fecha, horario):
+    print(f"Consultando disponibilidad para {laboratorio_id} en {fecha} - {horario}")
+    return True
